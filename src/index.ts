9dc516fc10e01ec7951a3c8653cb51bc32ca43a1
Grigore Gabriel Trifan <trifangaby71@protonmail.com>
2024-02-26 01:08:58 +0200

    add eth price fetch and conversion

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import env from "dotenv";
 import path from "path";
 // Replace if using a different env file or config.
 env.config({ path: "./.env" });
-
+import cors from "cors";
 import bodyParser from "body-parser";
 import express from "express";
-
+import { ethers } from "ethers";
+import { GENESIS_ABI, GENESIS_ADDRESS } from "./contractInfo";
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 	apiVersion: "2023-10-16",
@@ -21,7 +22,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 const app = express();
 const resolve = path.resolve;
 
-app.use(express.static(process.env.STATIC_DIR));
+app.use(cors());
+
 app.use(
 	(
 		req: express.Request,
@@ -48,19 +50,56 @@ app.get("/config", (_: express.Request, res: express.Response): void => {
 		publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
 	});
 });
+async function convertEthToEur(ethPrice: number) {
+	try {
+		const response = await fetch(
+			"https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=eur"
+		);
+		const data = await response.json();
 
+		const eurPrice = ethPrice * data.ethereum.eur;
+		return eurPrice;
+	} catch (err) {
+		console.error(err);
+		return null;
+	}
+}
+async function getCurrentPrice(): Promise<number> {
+	try {
+		const network = "homestead"; // MAINNET
+
+		const provider = ethers.getDefaultProvider(network, {
+			infura: process.env.INFURA_ID,
+		});
+
+		let GENESIS = new ethers.Contract(GENESIS_ADDRESS, GENESIS_ABI, provider);
+		let currentPrice = ethers.utils.formatEther(await GENESIS.currentPrice());
+
+		const currentPriceEur = await convertEthToEur(Number(currentPrice));
+		if (currentPriceEur !== null) {
+			console.log(
+				"Current price: " + currentPrice + " ETH or " + currentPriceEur + " EUR"
+			);
+			return currentPriceEur;
+		} else {
+			console.log("Unable to fetch EUR price");
+		}
+	} catch (err) {
+		console.log(err);
+	}
+}
 app.get(
 	"/create-payment-intent",
 	async (req: express.Request, res: express.Response): Promise<void> => {
+		const price: number = await getCurrentPrice();
 		// Create a PaymentIntent with the order amount and currency.
 		const params: Stripe.PaymentIntentCreateParams = {
-			amount: 1999,
+			amount: Math.round(Number(price * 100)),
 			currency: "EUR",
 			automatic_payment_methods: {
 				enabled: true,
 			},
 		};
-
 		try {
 			const paymentIntent: Stripe.PaymentIntent =
 				await stripe.paymentIntents.create(params);
@@ -68,6 +107,7 @@ app.get(
 			// Send publishable key and PaymentIntent client_secret to client.
 			res.send({
 				clientSecret: paymentIntent.client_secret,
+				amount: Math.round(Number(price)),
 			});
 		} catch (e) {
 			res.status(400).send({
